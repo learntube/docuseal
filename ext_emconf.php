2f9f47590bbbea51a8b340ed2e3dad4f64d8a6a0
Kallol Chakraborty <kallolchakraborty@hotmail.com>
2025-03-25 15:48:46 +0530

    [TASK] Allow extension for v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'kchakraborty@learntube.de',
     'author_company' => 'LEARNTUBE GmbH',
     'state' => 'stable',
-    'version' => '1.0.1',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '13.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
